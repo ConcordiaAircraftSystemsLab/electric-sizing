9ec7f97bc25a0a18fd81b75f12770a6ac68ea0da
Santiago Valencia <santiago.valencia8@gmail.com>
2024-07-29 16:13:09 -0400

    Add weight estimation

---
+++ electric_sizing/empty_weight_table.py
@@ -0,0 +1,22 @@
+AIRCRAFT_DATA = {
+    "Sailplane—unpowered": {"A-metric": 0.83, "C": -0.05},
+    "Sailplane—powered": {"A-metric": 0.88, "C": -0.05},
+    "Homebuilt—metal/wood": {"A-metric": 1.11, "C": -0.09},
+    "Homebuilt—composite": {"A-metric": 1.07, "C": -0.09},
+    "General aviation—single engine": {"A-metric": 2.05, "C": -0.18},
+    "General aviation—twin engine": {"A-metric": 1.4, "C": -0.10},
+    "Agricultural aircraft": {"A-metric": 0.72, "C": -0.10},
+    "Twin turboprop": {"A-metric": 0.92, "C": -0.03},
+    "Flying boat": {"A-metric": 1.05, "C": -0.05},
+    "Jet trainer": {"A-metric": 1.47, "C": -0.10},
+    "Jet fighter": {"A-metric": 2.11, "C": -0.13},
+    "Military cargo/bomber": {"A-metric": 0.88, "C": -0.07},
+    "Jet transport": {"A-metric": 0.97, "C": -0.06},
+    "UAV—Tac Recce & UCAV": {"A-metric": 1.47, "C": -0.16},
+    "UAV—high altitude": {"A-metric": 2.39, "C": -0.18},
+    "UAV—small": {"A-metric": 0.93, "C": -0.06},
+}
+
+
+def get_empty_weight_fraction_params(aircraft_type: str):
+    return AIRCRAFT_DATA.get(aircraft_type, "Aircraft type not found")
---
+++ electric_sizing/weight_estimation.py
@@ -0,0 +1,51 @@
+from empty_weight_table import get_empty_weight_fraction_params
+
+
+def get_gross_takeoff_weight(
+    crew_weight: float,
+    payload_weight: float,
+    empty_weight_fraction: float,
+    fuel_weight_fraction: float,
+):
+    """
+    Calculate the gross takeoff weight of an aircraft based on weight fractions and payload.
+
+    Args:
+        crew_weight (float): The weight of the crew in kg.
+        payload_weight (float): The weight of the payload in kg.
+        empty_weight_fraction (float): The fraction of empty weight to gross weight.
+        fuel_weight_fraction (float): The fraction of fuel weight to gross weight.
+
+    Returns:
+        float: The calculated gross takeoff weight of the aircraft.
+    """
+
+    return (crew_weight + payload_weight) / (
+        1 - fuel_weight_fraction - empty_weight_fraction
+    )
+
+
+def get_empty_weight_fraction(
+    gross_takeoff_weight: float,
+    aircraft_type: str,
+    variable_sweep: bool = False,
+):
+    """
+    Calculate the empty weight fraction of an aircraft based on the gross takeoff weight and aircraft type.
+
+    Args:
+        gross_takeoff_weight (float): The gross takeoff weight of the aircraft in kg.
+        aircraft_type (str): The type of the aircraft.
+        variable_sweep (bool, optional): Indicates if the aircraft has variable sweep wings. Default is False.
+
+    Returns:
+        float: The calculated empty weight fraction of the aircraft.
+    """
+
+    regression_params = get_empty_weight_fraction_params(aircraft_type)
+
+    return (
+        regression_params["A-metric"]
+        * (gross_takeoff_weight ** regression_params["C"])
+        * (1.04 if variable_sweep else 1)
+    )
